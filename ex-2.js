cbc109272d96cc0943bdf3d7236253043e926dfb
S33N <145544074+rizzboislw@users.noreply.github.com>
2024-07-24 14:22:10 +0700

    ex-2

--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,26 @@
 function sortStudentsByScore(students) {
-  // เริ่มเขียนโค้ดตรงนี้จ้า
+  for (let i = 0; i < students.length; i++) {
+    for (let j = 0; j < students.length - 1 - i; j++) {
+      if (students[j].score < students[j + 1].score) {
+        let temp = students[j];
+        students[j] = students[j + 1];
+        students[j + 1] = temp;
+      }
+    }
+  }
+  return students;
 }
+
+let studentScores = [
+  { id: "S001", name: "Anan", score: 85 },
+  { id: "S002", name: "Boon", score: 92 },
+  { id: "S003", name: "Chai", score: 78 },
+  { id: "S004", name: "Dao", score: 95 },
+  { id: "S005", name: "Ek", score: 88 },
+  { id: "S006", name: "Fah", score: 80 },
+];
+
+console.log(sortStudentsByScore(studentScores));
+
+// Time Complexity = O(n^2)
+// เนื่องจากมีการใช้ Bubble Sort ซึ่งเป็นการ Sort โดยใช้ nested loop จึงได้ออกมาเป็น O(n^2)
